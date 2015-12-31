6a80e1c5b6e1abdce8e525a5a501cb65d68052d2
YJXie <xieyunjia@foxmail.com>
2015-12-31 15:26:08 +0800

    Initial Commit

---
+++ RFTShareView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RFTShareView
+//
+//  Created by YJXie on 15/12/31.
+//  Copyright © 2015年 YJXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RFTShareView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RFTShareView
+//
+//  Created by YJXie on 15/12/31.
+//  Copyright © 2015年 YJXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
